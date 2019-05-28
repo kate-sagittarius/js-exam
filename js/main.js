26a87fdb40442bf94763adc689e9bfbe03310087
Kate-Sagittarius <kate.zakhovaiko@gmail.com>
2019-05-28 16:37:24 +0300

    refoctor (change "song" to "track", optimize api requests links)

--- js/main.js
+++ js/main.js
@@ -2,91 +2,133 @@ const musixMatchKey = 'dd09d77faf4b4cfe40a00dd542b68f74';
 
 const corsProxyUrl = 'https://cors-anywhere.herokuapp.com';
 
-const songsApiUrl = (option, text) => {
-  const tempUrl = `q_${option}=${text}`;
+const encodeQueryParams = (params) => {
+  return Object.keys(params).map((key) => {
+    const value = params[key];
+    return `${key}=${encodeURIComponent(value)}`;
+  }).join('&');
+};
 
-  return `${corsProxyUrl}/api.musixmatch.com/ws/1.1/track.search?${tempUrl}&page_size=5&page=1&s_track_rating=desc&apikey=${musixMatchKey}`.replace(/\s/g, '%20');
+const musixMatchUrl = (action, params) => {
+  const encodedParams = encodeQueryParams({
+    apikey: musixMatchKey,
+    ...params
+  });
+  return `${corsProxyUrl}/api.musixmatch.com/ws/1.1/${action}?${encodedParams}`;
 }
 
-const getApiSongData = (x) => {
+const trackSearchUrl = (option, text) => {
+  return musixMatchUrl('track.search', {
+    [`q_${option}`]: text,
+    page_size: 5,
+    page: 1,
+    s_track_rating: 'desc',
+  });
+};
+
+const trackLyricsUrl = (trackId) => {
+  return musixMatchUrl('track.lyrics.get', {
+    track_id: trackId,
+  });
+};
+
+const wikipediaUrl = (params) => {
+  const encodedParams = encodeQueryParams(params);
+  return `${corsProxyUrl}/en.wikipedia.org/w/api.php?${encodedParams}`;
+};
+
+const getApiTrackData = (x) => {
   const {
     artist_name: artistName,
-    track_name: songName,
+    track_name: trackName,
     album_name: albumName,
     num_favourite: likesNumber,
-    track_id: songId,
+    track_id: trackId,
   } = x.track;
-  return { artistName, songName, albumName, likesNumber, songId };
+  return { artistName, trackName, albumName, likesNumber, trackId };
 }
 
-const getSongInfo = (option, text) => {
-  return axios.get(songsApiUrl(option, text)).then(response => {
-    return response.data.message.body.track_list.map((x) => getApiSongData(x))
-  })
-}
+const getTrackInfo = (option, text) => {
+  return axios.get(trackSearchUrl(option, text)).then(response => {
+    return response.data.message.body.track_list.map((x) => getApiTrackData(x))
+  });
+};
 
 const getWikipediaImageUrl = (filename) => {
-  return axios.get(`${corsProxyUrl}/en.wikipedia.org/w/api.php?format=json&action=query&titles=Image:${encodeURIComponent(filename)}&prop=imageinfo&iiprop=url`)
-    .then((response) => {
-      const pages = Object.keys(response.data.query.pages)[0];
-      return response.data.query.pages[pages].imageinfo[0].url;
-    });
+  return axios.get(wikipediaUrl({
+    format: 'json',
+    action: 'query',
+    titles: `Image:${filename}`,
+    prop: 'imageinfo',
+    iiprop: 'url',
+  })).then((response) => {
+    const pages = Object.keys(response.data.query.pages)[0];
+    return response.data.query.pages[pages].imageinfo[0].url;
+  });
 };
 
 const getArtistInfo = (artistName) => {
-  return axios.get(`${corsProxyUrl}/en.wikipedia.org/w/api.php?format=json&action=query&prop=images|extracts&exintro&explaintext&redirects=1&titles=${artistName}`)
-    .then(response => {
-      const wikiPages = Object.keys(response.data.query.pages)[0];
-      const page = response.data.query.pages[wikiPages];
+  return axios.get(wikipediaUrl({
+    format: 'json',
+    action: 'query',
+    prop: 'images|extracts',
+    exintro: '',
+    explaintext: '',
+    redirects: 1,
+    titles: artistName,
+  })).then(response => {
+    const wikiPages = Object.keys(response.data.query.pages)[0];
+    const page = response.data.query.pages[wikiPages];
 
-      const eligibleImages = page.images.filter((x) => !x.title.includes('.svg') && !x.title.includes('.ogg'));
-      const imageFilename = eligibleImages[Math.floor(Math.random() * eligibleImages.length)].title.slice(5); // drop "File:"
+    const eligibleImages = page.images.filter((x) => !x.title.includes('.svg') && !x.title.includes('.ogg'));
+    const imageFilename = eligibleImages[Math.floor(Math.random() * eligibleImages.length)].title.slice(5); // slice(5) = drop "File:"
 
-      return getWikipediaImageUrl(imageFilename).then((artistPic) => {
-        const artistBio = page.extract.split('\n')[0];
-        return {artistPic, artistBio};
-      });
-  });
-}
+    return getWikipediaImageUrl(imageFilename).then((artistPic) => {
+      const artistBio = page.extract.split('\n')[0];
+      return {artistPic, artistBio};
+    });
+});
+};
 
 document.getElementById('search-form').onsubmit = (event) => {
   event.preventDefault();
 
   const userOption = document.getElementById('select').value;
   const userText = document.getElementById('query').value;
-  const songInfo = getSongInfo(userOption, userText);
+  const trackInfo = getTrackInfo(userOption, userText);
 
-  songInfo.then((songsList) => {
-    console.log(songsList);
-    const artistName = songsList[0].artistName;
+  trackInfo.then((tracksList) => {
+    console.log(tracksList);
+    const artistName = tracksList[0].artistName;
     getArtistInfo(artistName).then((artistInfoResult) => {
       console.log(artistInfoResult);
-      createHTMLStructure(artistName, artistInfoResult.artistPic, artistInfoResult.artistBio, songsList);
-    })
-  })
-}
+      createHTMLStructure(artistName, artistInfoResult.artistPic, artistInfoResult.artistBio, tracksList);
+    });
+  });
+};
 
-
-const getSongLyrics = (songId) => {
-  return axios.get(`${corsProxyUrl}/api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${songId}&apikey=${musixMatchKey}`)
+const getTrackLyrics = (trackId) => {
+  return axios.get(trackLyricsUrl(trackId))
     .then(response => {
       return response.data.message.body.lyrics.lyrics_body;
     });
-}
-
+};
 
 const main = document.getElementById('main');
 
-
-const createHTMLStructure = (artistName, artistPic, artistBio, songList) => {
+const createHTMLStructure = (artistName, artistPic, artistBio, trackList) => {
   main.innerHTML = '';
+  const sectionAuthor = createElement('section', "main__artist", main);
+  sectionAuthor.setAttribute('id', 'section-author');
 
   createArtistHTMLStructure(artistName, artistPic, artistBio);
-  createSongListHTMLStructure(songList);
+  createTrackListHTMLStructure(trackList);
 };
 
 const createArtistHTMLStructure = (artistName, artistPic, artistBio) => {
-  const sectionAuthor = createElement('section', "main__artist");
+  const sectionAuthor = document.getElementById('section-author');
+  sectionAuthor.innerHTML = '';
+
   const imgAuthor = createElement('img', "main__artist__image main__artist__image--downloaded", sectionAuthor);
   imgAuthor.setAttribute("alt", artistName);
   imgAuthor.setAttribute("src", artistPic);
@@ -96,45 +138,45 @@ const createArtistHTMLStructure = (artistName, artistPic, artistBio) => {
   pArtistBio.innerHTML = artistBio;
 };
 
-const createSongListHTMLStructure = (songList) => {
-  const sectionSongs = createElement('section', "main__songs");
-  const olSongs = createElement('ol', "main__songs__list", sectionSongs);
-  songList.forEach(x => {
-    const liSong = createElement('li', "main__songs__list__item", olSongs);
-    liSong.onclick = () => {
-      showSongLyrics(liSong, x.songId);
+const createTrackListHTMLStructure = (trackList) => {
+  const sectionTracks = createElement('section', "main__tracks", main);
+  const olTracks = createElement('ol', "main__tracks__list", sectionTracks);
+  trackList.forEach(x => {
+    const liTrack = createElement('li', "main__tracks__list__item", olTracks);
+    liTrack.onclick = () => {
+      showTrackLyrics(liTrack, x.trackId);
+      updateArtist(x.artistName);
     }
-    const hSong = createElement('h4', "main__songs__list__item__name", liSong);
-    hSong.innerHTML = `${x.songName} (${x.albumName})`;
-    const divSongLikes = createElement('div', "main__songs__list__item__likes", liSong);
-    const imgLike = createElement('img', "like-image", divSongLikes);
+    const hTrack = createElement('h4', "main__tracks__list__item__name", liTrack);
+    hTrack.innerHTML = `${x.trackName} (${x.albumName})`;
+    const divTrackLikes = createElement('div', "main__tracks__list__item__likes", liTrack);
+    const imgLike = createElement('img', "like-image", divTrackLikes);
     imgLike.setAttribute("alt", "like");
     imgLike.setAttribute("src", "images/like.svg");
-    const pLikesNumber = createElement('p', "likes-number", divSongLikes);
+    const pLikesNumber = createElement('p', "likes-number", divTrackLikes);
     pLikesNumber.innerHTML = x.likesNumber;
   });
 };
 
-const createElement = (element, classCSS, parent = main) => {
+const createElement = (element, classCSS, parent) => {
   const tag = document.createElement(element);
   tag.setAttribute("class", classCSS);
   parent.appendChild(tag);
   return tag;
 };
 
-const showSongLyrics = (liSong, songId) => {
-  const lyrics = [...document.getElementsByClassName("main__songs__list__item__lyrics")];
+const showTrackLyrics = (liTrack, trackId) => {
+  const lyrics = [...document.getElementsByClassName("main__tracks__list__item__lyrics")];
   lyrics.forEach(x => x.remove());
 
-  const pSong = createElement('p', "main__songs__list__item__lyrics", liSong);
-  getSongLyrics(songId).then((lyrics) => {
-    pSong.innerHTML = lyrics;
+  const pTrack = createElement('p', "main__tracks__list__item__lyrics", liTrack);
+  getTrackLyrics(trackId).then((lyrics) => {
+    pTrack.innerHTML = lyrics;
   });
 };
 
-// const updateArtist = (artistName) => {
-//   // get artist info
-//   getArtistInfo(artistName).then((info) => {
-//     // display artist info
-//   });
-// };
+const updateArtist = (artistName) => {
+  getArtistInfo(artistName).then((info) => {
+    createArtistHTMLStructure(artistName, info.artistPic, info.artistBio);
+  });
+};
